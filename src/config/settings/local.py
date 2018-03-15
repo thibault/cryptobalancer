18557f374f04b4158214a9bff93513ece697a819
Thibault Jouannic <thibault@miximum.fr>
2018-03-15 16:53:24 +0100

    Update allowed hosts settings

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -9,6 +9,7 @@ SECRET_KEY = 'elfiyBaxB1Mtj4kaCE1cEQC2LholgfnrdiYGGrfIvST3CSfl18Q6sHNho18TB1oE'
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
     "localhost",
+    "cryptobalancer",
     "0.0.0.0",
 ]
 
