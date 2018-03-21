349dfb7ee573a861f5cea5b4b7257c7e782050c6
Thibault Jouannic <thibault@miximum.fr>
2018-03-21 14:40:57 +0100

    Better "help text" message in form

--- src/portfolio/forms.py
+++ src/portfolio/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Position
 
@@ -8,3 +9,9 @@ class PositionForm(forms.ModelForm):
     class Meta:
         exclude = []
         model = Position
+        help_texts = {
+            'target': _('How much does this asset should weight in your \
+                        portfolio? If the sum of all "target" values is not \
+                        equal to 100, we will automatically compute the \
+                        corresponding percentages.')
+        }
